3138686db118f46f0a990729e4a68b6c482bcc07
David P. Chassin <david.chassin@me.com>
2021-09-27 06:13:37 -0700

    Update math.py

--- src/math.py
+++ src/math.py
@@ -24,9 +24,15 @@ GLOBALS
 
     answer    list of answers
 
+    copyright copyright statement
+
+    credits   authoring credits
+
     debug     boolean indicating whether debug is enabled
 
     history   list of inputs
+
+    license   license agreement
 
     prompt    interactive input prompt
 
@@ -35,11 +41,44 @@ GLOBALS
 
 import sys
 
-debug = False
+copyright = "Copyright (C) 2021, David P. Chassin"
+credits = "Written by David P. Chassin"
 version = "0.1.0"
 prompt = "math> "
+debug = False
 answer = []
 history = []
+license = """
+BSD 3-Clause License
+
+Copyright (c) 2021, David P. Chassin
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
 
 if "-h" in sys.argv or "--help" in sys.argv or "help" in sys.argv:
     if sys.stdin.isatty():
